7cf5b5fc2d1fe761846c9e453456141edc747f56
FebrianAditya <febrian.aditya@student.upi.edu>
2025-01-16 05:17:02 +0700

    change import

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,24 +1,24 @@
 // import './App.css'
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
-import HomePage from './pages/HomePage/HomePage'
-import AboutPage from './pages/aboutPage/AboutPage'
-import AboutCompany from "./pages/AllAbout/AboutCompany"
-import AboutMePage from "./pages/AllAbout/AboutMePage" 
+import HomePage from './pages/HomePage/HomePage.jsx'
+import AboutPage from './pages/aboutPage/AboutPage.jsx'
+import AboutCompany from "./pages/AllAbout/AboutCompany.jsx"
+import AboutMePage from "./pages/AllAbout/AboutMePage.jsx" 
 // import FebirPage from "./pages/memberPage/FebriPage"
 // import JhonPage from "./pages/memberPage/JhonPage"
-import OnlyMember from "./pages/memberPage/onlyMemberPage"
-import CarPage from "./pages/CarsPage/CarsPage"
-import DetailCar from "./pages/CarsPage/DetailCar"
-import ProtectecRoute from "./components/ProtectedRoute"
-import UserPage from "./pages/UserPage"
+import OnlyMember from "./pages/memberPage/onlyMemberPage.jsx"
+import CarPage from "./pages/CarsPage/CarsPage.jsx"
+import DetailCar from "./pages/CarsPage/DetailCar.jsx"
+import ProtectecRoute from "./components/ProtectedRoute.jsx"
+import UserPage from "./pages/UserPage.jsx"
 // import DemoPage from "./pages/DemoPage"
 
 // import { Routes, Route } from "react-router-dom"
 import { Route, Routes } from "react-router-dom"
-import NavbarComponent from './components/NavbarComponent'
-import MemberPage from "./pages/memberPage/MemberPage"
-import DashboardPage from "./pages/DashboardPage"
+import NavbarComponent from './components/NavbarComponent.jsx'
+import MemberPage from "./pages/memberPage/MemberPage.jsx"
+import DashboardPage from "./pages/DashboardPage.jsx"
 import { useState } from "react"
 
 
